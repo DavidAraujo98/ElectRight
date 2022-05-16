a4ce103a340f27badd4b4cae5c363271744adaf6
David Araújo <david2araujo5@gmail.com>
2022-05-17 00:14:27 +0100

    Router implemented

--- elect-right/src/App.js
+++ elect-right/src/App.js
@@ -1,11 +1,31 @@
-import React from 'react'
 import './App.css';
+import React from 'react'
 import Navbar from './components/Navbar';
-import user_img from './assets/user-solid.svg';
+import Home from './pages/Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <Navbar username="John Doe" img_src={user_img} />
+    <Router>
+      <div className="App">
+        <Navbar username="John Doe" img_src='./assets/user-solid.svg' />
+        <div className="Content">
+          <Switch>
+            {/*
+            <Route exact path="/login">
+              <Login />
+            </Route>
+            <Route exact path="/signup">
+              <SignUp />
+            </Route>
+            */}
+            <Route exact path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
