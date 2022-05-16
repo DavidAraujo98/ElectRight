a70a9fa9b2312dbc03e251f6c921013bfeaed7e8
David Araujo <david2araujo5@gmail.com>
2022-05-16 11:37:04 +0100

    Update

---
+++ elect-right/src/components/Navbar.js
@@ -0,0 +1,11 @@
+const Navbar = () => {
+    return ( 
+        <nav class="navbar bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="/">ElectRight</a>
+            </div>
+        </nav>
+    );
+}
+ 
+export default Navbar;
