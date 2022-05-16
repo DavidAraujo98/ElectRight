7462dd2b0d572a6b8eb0656b5f1fe84b1ed42f13
David Araujo <david2araujo5@gmail.com>
2022-05-16 13:29:03 +0100

    Components update

--- elect-right/src/components/Navbar.js
+++ elect-right/src/components/Navbar.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from 'react';
+import Title from './Title';
 
 const Navbar = () => {
     return ( 
         <nav class="navbar bg-light">
             <div class="container-fluid">
-                <a class="navbar-brand" href="/">ElectRight</a>
+                <a class="navbar-brand" href="/"><Title /></a>
             </div>
         </nav>
     );
---
+++ elect-right/src/components/Title.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Title = (props) => {
+    return (
+        <div>
+            <h1><span class="green">Elect</span><span class="pink">Right</span></ h1>
+        </div>
+    );
+}
+ 
+export default Title;
