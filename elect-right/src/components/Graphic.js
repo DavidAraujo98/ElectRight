f7239315a8b6a6a53bba2b9920626c351ba1f6b1
Catarina Oliveira <catarinacruzoliveira01@ua.pt>
2022-06-07 17:45:44 +0100

    results, needs reform

--- elect-right/src/components/Graphic.js
+++ elect-right/src/components/Graphic.js
@@ -1,15 +1,27 @@
-import React from 'react';
-import {Doughnut} from 'react-chartjs-2'; // npm install react-chartjs-2
+import React, {useState} from 'react';
+import {Doughnut} from 'react-chartjs-2';
+import {Chart, ArcElement} from 'chart.js'
+
+import results from "../pages/Results"; // npm install react-chartjs-2
+
+Chart.register(ArcElement);
+const Graphic = (props) => {
+
+    let nVotesTrue = []
+    props.users.forEach((voter) => nVotesTrue.push(voter.votes.filter(vote => (vote.id === props.idProp && vote.value))));
+    nVotesTrue = nVotesTrue.filter(result => result.length !== 0)
+
+    let nVotesFalse = []
+    props.users.forEach((voter) => nVotesFalse.push(voter.votes.filter(vote => (vote.id === props.idProp && !vote.value))));
+    nVotesFalse = nVotesFalse.filter(result => result.length !== 0)
+
+    let nVotes = []
+    props.users.forEach((voter) => nVotes.push(voter.votes.filter((vote) => vote.id === props.idProp)));
+    nVotes = nVotes.filter(result => result.length !== 0)
 
 
-const Graphic = ({users, idProp}) => {
-
-    const nVotesTrue = users.votes.filter(voter => (voter.id === idProp && voter.value === true)).length;
-    const nVotesFalse = users.votes.filter(voter => (voter.id === idProp && voter.value === false)).length;
-    const nVotes = users.length;
-    console.log(nVotesTrue*100/nVotes);
-
-    {/* const data = {
+    const data = {
+        id: 160,
         labels: ['Accept', 'Declined'],
         datasets: [
             {
@@ -18,7 +30,7 @@ const Graphic = ({users, idProp}) => {
                 title: {
                     text: "Customer Satisfaction"
                 },
-                data: [(nVotesTrue*100/nVotes) , (nVotesFalse*100/nVotes)],
+                data: [(nVotesTrue.length*100/nVotes.length) , (nVotesFalse.length*100/nVotes.length)],
                 backgroundColor: [
                     'rgba(54, 162, 235, 0.5)',
                     'rgba(54, 162, 235, 0.2)',
@@ -30,12 +42,13 @@ const Graphic = ({users, idProp}) => {
                 borderWidth: 1,
             },
         ],
-    }; */}
+    };
 
     return (
-        <dic>
-            {/*     <Doughnut data={data}/> */}
-        </dic>
+        <div>
+            <Doughnut data={data}/>
+
+        </div>
     );
 }
 
--- elect-right/src/pages/Results.js
+++ elect-right/src/pages/Results.js
@@ -2,17 +2,29 @@ import Graphic from "../components/Graphic";
 import React from "react";
 import {useState} from "react";
 import UserToast from "../components/UserToast";
+import {Col, Container, Row} from "react-bootstrap";
 
 const Results = () => { // elect
 
     const [elect] = useState(
-        {"id":'AS23D1', 'title':"Children's Play PArk Project", 'data':'10-01-22', 'voters':[{"id":1233241, "name":"Leonardo"}, {"id":6367756, "name":"Carolina"}, {"id":9490184, "name":"Otávio", "votes":[{
-        "id": "AS23D2", "value": true }]}, {"id":1233241, "name":"Urtélia", "votes":[{ "id": "AS23D2", "value": false } ]}]}
-        );
+        {
+            "id": 'AS23D1',
+            'title': "Children's Play PArk Project",
+            'startDate': '2022-06-07T00:45',
+            'endDate': '2022-06-07T00:50',
+            'proposals': [{'id': "AS23D2", 'title': 'New swings', "old_": 'new', 'new_': 'new new', 'images': []}],
+            'voters': [{"id": 1233241, "name": "Leonardo"}, {"id": 6367756, "name": "Carolina"}, {
+                "id": 9490184, "name": "Otávio", "votes": [{
+                    "id": "AS23D2", "value": true
+                }]
+            }, {"id": 1233241, "name": "Urtélia", "votes": [{"id": "AS23D2", "value": false}]}]
+        }
+    );
 
-    const [users] = useState(elect.voters.filter(voter => (voter.votes !== undefined)));
-    console.log(users.length)
-    //const {code, name} = useParams();
+
+    const [voters] = useState(elect.voters.filter(voter => (voter.votes !== undefined)));
+    const [proposals] = useState(elect.proposals);
+
 
     return (
         <div className="results">
@@ -20,20 +32,26 @@ const Results = () => { // elect
                 <h2>{elect.title}</h2>
                 <h3>Session:{elect.id}</h3>
             </div>
-            <div>
-                 <div>
-                      <h4>Results</h4>
-                     {elect.voters.filter(voter => (voter.votes !== undefined)).map((user) => (
-                         <Graphic users={users} idProp={ user.votes.id } />
-                     ))}
-                </div>
-                <div>
-                    <h4>Votes</h4>
-                    {elect.voters.filter(voter => (voter.votes !== undefined)).map((user) => (
-                        <UserToast user={user} />
-                    ))}
-                </div>
-            </div>
+            <Container>
+                <Row>
+                    <Col>
+                        <h4>Results</h4>
+                        {proposals.map((proposal) => (
+                            <div>
+                                <h4>{proposal.title}</h4>
+                                <Graphic users={voters} idProp={proposal.id}/>
+                            </div>
+                        ))}
+
+                    </Col>
+                    <Col>
+                        <h4>Votes</h4>
+                        {elect.voters.filter(voter => (voter.votes !== undefined)).map((user) => (
+                            <UserToast user={user}/>
+                        ))}
+                    </Col>
+                </Row>
+            </Container>
         </div>
 
     );
