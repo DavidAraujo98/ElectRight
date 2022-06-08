5ae12e3a676fe13ce1b786cf04fa0c3c6e0804d9
David Araújo <david2araujo5@gmail.com>
2022-06-08 20:28:44 +0100

    Home page complete

--- elect-right/src/pages/Home.js
+++ elect-right/src/pages/Home.js
@@ -1,4 +1,3 @@
-import Carousel from "react-bootstrap/Carousel"; // npm install react-bootstrap
 import "../css/Home.css";
 import React, { useState } from "react";
 
@@ -6,75 +5,68 @@ const Home = () => {
   const [code, setCode] = useState("");
 
   return (
-    <div className="home">
-      <Carousel>
-        <Carousel.Item interval={1000}>
-          <img
-            className="d-block w-100"
-            src="https://webassets.oxfamamerica.org/media/images/VoteVoice-web-2440x1526.width-1200.png"
-            alt="First slide"
-          />
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item interval={500}>
-          <img
-            className="d-block w-100"
-            src="https://m.media-amazon.com/images/S/aplus-media/vc/a6736b2f-2cde-46a5-8eab-c44e7553f74f.__CR0,0,970,300_PT0_SX970_V1___.png"
-            alt="Second slide"
-          />
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="https://conteudo.solutudo.com.br/wp-content/uploads/2022/01/futuro-da-trabalho-cobra-CIOs.jpg"
-            alt="Third slide"
-          />
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
-      <div className="row mt-4 zindex-modal">
-        <div className="col-6 area-pink bg-opacity-50 rounded-4 ms-sm-5 m-auto p-3">
-          <div className="text-sm-start mb-2">
-            <h1>Election taking place?</h1>
-            <label>
-              Insert the election code bellow to access the voting ballot.
-            </label>
-            <input
-              value={code}
-              onChange={(e) => setCode(e.target.value)}
-              type="text"
-              id="code"
-              placeholder="Election code"
-              className="form-control shadow-none pink fs-1 border border-0 rounded-pill px-4 m-auto"
-            />
+      <div className="home">
+          <div
+              id="carouselExampleSlidesOnly"
+              className="carousel slide"
+              data-bs-ride="carousel"
+          >
+              <div className="carousel-inner m-0">
+                  <div className="carousel-item active">
+                      <img
+                          src={require("../assets/voting1.jpg")}
+                          alt="..."
+                          className="min-vw-100 vh-100"
+                      />
+                  </div>
+                  <div className="carousel-item">
+                      <img
+                          src={require("../assets/voting2.jpg")}
+                          alt="..."
+                          className="min-vw-100 vh-100"
+                      />
+                  </div>
+                  <div className="carousel-item">
+                      <img
+                          src={require("../assets/voting3.jpg")}
+                          alt="..."
+                          className="min-vw-100 vh-100"
+                      />
+                  </div>
+              </div>
           </div>
-          <div className="text-sm-end">
-            <a
-              href={"ballot?idb=" + code}
-              className={
-                code == ""
-                  ? "btn btn-elect rounded-pill disabled"
-                  : "btn btn-elect rounded-pill"
-              }
-            >
-              Access
-            </a>
+          <div id="searchcode" className="row mt-4 fw-light">
+              <div className="col-5 area-pink rounded-4 ms-sm-5 m-auto p-3">
+                  <div className="text-sm-start mb-2">
+                      <h1 className="my-2">Election taking place?</h1>
+                      <label className="my-2">
+                          Insert the election code bellow to access the voting
+                          ballot.
+                      </label>
+                      <input
+                          value={code}
+                          onChange={(e) => setCode(e.target.value)}
+                          type="text"
+                          id="code"
+                          placeholder="Election code"
+                          className="form-control shadow-none pink fs-1 border border-0 rounded-pill px-4 my-2"
+                      />
+                  </div>
+                  <div className="text-sm-end">
+                      <a
+                          href={"ballot?idb=" + code}
+                          className={
+                              code == ""
+                                  ? "btn btn-elect rounded-pill disabled"
+                                  : "btn btn-elect rounded-pill"
+                          }
+                      >
+                          Access
+                      </a>
+                  </div>
+              </div>
           </div>
-        </div>
       </div>
-    </div>
   );
 };
 
